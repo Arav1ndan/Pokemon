a4b5f62223a964d11143f9eb1cb23447dd75138e
Arav1ndan <aravindanputhalath@gmail.com>
2024-11-18 22:05:51 +0530

    The Hidden Grasslands

---
+++ grass.hpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+#include "PokemonType.hpp"
+
+using namespace std;
+class Pokemon;
+enum class PokemonType;
+struct Grass
+{
+    string enivormentType;
+    vector<Pokemon> wildPokemonList;
+    int encounterRate;
+};
+Grass forestGrass = {
+    "Forest",{{"Pidgey", PokemonType::NORMAL, 40}, {"Caterpie", PokemonType::BUG, 35}}, 70};
+Grass caveGrass = {
+    "Cave",
+    {{"Zubat", PokemonType::POISON, 30}, {"Geodude", PokemonType::ROCK, 50}},
+    80};
