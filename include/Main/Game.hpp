754b64e1adeeefd550b34a8edb3824fa77cd3dba
Arav1ndan <aravindanputhalath@gmail.com>
2024-11-21 16:13:35 +0530

    Organize Your Project Files

---
+++ include/Main/Game.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include "../../include/Pokemon/Grass.hpp"
+class Player;
+
+class Game {
+private:
+  Grass forestGrass;
+public:
+  Game();
+  void gameLoop(Player &player);
+  void visitPokeCenter(Player &player); 
+};
