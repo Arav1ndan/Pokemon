3837d703425fc1febe6393d61d24af8c8b8fde8e
Arav1ndan <aravindanputhalath@gmail.com>
2024-11-22 12:45:50 +0530

    Implement Abstraction

--- include/Pokemon/Caterpie.hpp
+++ include/Pokemon/Caterpie.hpp
@@ -1,13 +1,18 @@
 #pragma once
 #include "../Pokemon.hpp"
 
-namespace N_Pokemon {
-  namespace N_Pokemons {
-    
-    class Caterpie : public Pokemon {
+namespace N_Pokemon
+{
+  namespace N_Pokemons
+  {
+
+    class Caterpie : public Pokemon
+    {
+    private:
+      void bugBite(Pokemon &target);
+
     public:
       Caterpie();
-      void bugBite(Pokemon &target);
     };
 
   }
