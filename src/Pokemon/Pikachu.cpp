61d2e749418a428fe3546b37f636710985d02619
Arav1ndan <aravindanputhalath@gmail.com>
2024-11-27 11:53:45 +0530

    Integrating Status Effects into the Battle System

--- src/Pokemon/Pikachu.cpp
+++ src/Pokemon/Pikachu.cpp
@@ -3,25 +3,23 @@
 #include "../../../include/Pokemon/Move.hpp"
 #include <iostream>
 
-namespace N_Pokemon {
-    namespace N_Pokemons {
-      
+namespace N_Pokemon
+{
+    namespace N_Pokemons
+    {
+
         using namespace std;
 
         Pikachu::Pikachu()
-                : Pokemon("Pikachu", PokemonType::ELECTRIC, 100, {
-                    Move("THUNDER SHOCK", 20),
-                    Move("QUICK ATTACK", 10),
-                    Move("THUNDER BOLT", 80)
-                }) {}
-        
-        void Pikachu::attack(Move selectedMove, Pokemon* target)
+            : Pokemon("Pikachu", PokemonType::ELECTRIC, 100, {Move("THUNDER SHOCK", 20), Move("QUICK ATTACK", 10), Move("THUNDER BOLT", 80)}) {}
+
+        void Pikachu::attack(Move selectedMove, Pokemon *target)
         {
-            if(selectedMove.name == "THUNDER BOLT")
+            if (selectedMove.name == "THUNDER BOLT")
             {
                 // 80% chance to hit
                 if (rand() % 100 < 80)
-                { 
+                {
                     Pokemon::attack(selectedMove, target);
                     std::cout << "... and it hit successfully!\n";
                 }
@@ -30,7 +28,14 @@ namespace N_Pokemon {
             }
             else
                 Pokemon::attack(selectedMove, target);
-            
+
+            if (selectedMove.name == "THUNDER SHOCK")
+            {
+
+                if (target->canApplyEffect())
+
+                    target->applyEffect(StatusEffectType::PARALYZED);
+            }
         }
-  }
+    }
 }
